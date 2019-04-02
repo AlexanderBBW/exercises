252200745d1fcf10c531e0d114fd955867948155
AlexanderBBW <alexander_grunert@gmx.de>
2019-04-02 10:40:01 +0200

    Aktualisierung der Übungen
    + Zahlenvergleich

---
+++ ZahlenVergleich/NUnit/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Zahlenvergleich;
+
+namespace NUnit
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class Test1
+        {
+            [Test, Category("Akzeptanztest")]
+            public static void VergleichZahlen()
+            {
+                var holeZahl = Program.ÜberprüfeZahl(5,6);
+                Assert.AreEqual(6, holeZahl);
+            }
+        }
+    }
+}
---
+++ ZahlenVergleich/Zahlenvergleich/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zahlenvergleich
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ersteZahl = ErfrageErsteZahl();
+            var zweiteZahl = ErfrageZweiteZahl();
+            var ergebnis = ÜberprüfeZahl(ersteZahl, zweiteZahl);
+            Console.WriteLine("Ergebnis: " + ergebnis);
+        }
+        public static int ErfrageErsteZahl()
+        {
+            Console.WriteLine("Geben Sie die erste Zahl ein!");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int ErfrageZweiteZahl()
+        {
+            Console.WriteLine("Geben Sie die zweite Zahl ein!");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int ÜberprüfeZahl(int ersteZahl, int zweiteZahl)
+        {
+            if(ersteZahl > zweiteZahl)
+                return ersteZahl;
+
+            return zweiteZahl;
+        }
+    }
+}
