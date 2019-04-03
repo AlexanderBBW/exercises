04f3b161c11958a850b08e321ad5ac77d42c5851
AlexanderBBW <alexander_grunert@gmx.de>
2019-04-03 10:54:32 +0200

    Neue Übungen
    + aktualisierung

---
+++ BildHochformatQuerformat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BildHochformatQuerformat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var breite = ErfrageBreite();
+            var höhe = ErfrageHöhe();
+            var hochformat = FormatAusgabe(höhe, breite);
+            Ausgabe(hochformat);
+        }
+        private static int ErfrageHöhe()
+        {
+            Console.WriteLine("Geben Sie die Höhe an");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        private static int ErfrageBreite()
+        {
+            Console.WriteLine("Geben Sie die Breite an");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static bool FormatAusgabe(int höhe, int breite)
+        {
+            return höhe > breite;
+        }
+        private static void Ausgabe(bool hochformat)
+        {
+            Console.WriteLine(hochformat ? "Hochformat" : "Querformat");
+        }
+    }
+}
---
+++ UNit/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BildHochformatQuerformat;
+
+namespace UNit
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class Test1
+        {
+            [Test, Category("Formattest")]
+            public static void FormatAusgaben()
+            {
+                var format = Program.FormatAusgabe(1080, 1920);
+                Assert.AreEqual(false, format);
+            }
+            [Test, Category("Formattest")]
+            public static void FormatAusgaben2()
+            {
+                var format = Program.FormatAusgabe(1920, 1080);
+                Assert.AreEqual(true, format);
+            }
+        }
+    }
+}
