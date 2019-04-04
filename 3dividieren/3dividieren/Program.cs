fa122f15467586f7cb4f6c4a7b40a821d705aa0b
AlexanderBBW <alexander_grunert@gmx.de>
2019-04-04 10:48:49 +0200

    Update
    + Erweiterungen

---
+++ 3dividieren/3dividieren/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3dividieren
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int min = 1;
+            const int max = 100;
+            var zähler = Berechne(min, max);
+            Ausgabe(zähler);
+        }
+
+        public static int Berechne(int min, int max)
+        {
+            var zähler = 0;
+            for (var iteration = min; iteration < max; iteration++)
+            {
+                if (iteration % 3 == 0)
+                {
+                    zähler++;
+                    iteration++;
+                }
+                iteration++;
+            }
+            return zähler;
+        }
+
+        public static void Ausgabe(int zähler)
+        {
+            Console.WriteLine(zähler);
+        }
+    }
+}
---
+++ 3dividieren/NUnit/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; 
+using _3dividieren;
+
+namespace NUnit
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class Test1
+        {
+            [Test, Category("Akzeptanstest")]
+            public static void DividierenÜberprüfung()
+            {
+                var Zahl = Program.Berechne(1, 100); 
+                Assert.AreEqual(33, Zahl);
+            }
+        }
+    }
+}
