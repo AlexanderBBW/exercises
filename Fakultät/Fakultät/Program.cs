408d1737e281b7cfada9a52271431a023ba4649a
AlexanderBBW <alexander_grunert@gmx.de>
2019-04-05 11:32:35 +0200

    Weiterarbeit
    +Ergänzungen und Erweiterungen

---
+++ Fakultät/Fakultät/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fakultät
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eingabe = HoleZahlenEingabe();
+            var ergebnis =  Berechnung(eingabe);
+            ZeigeErgebnis(ergebnis);
+        }
+
+        public static int HoleZahlenEingabe()
+        {
+            Console.WriteLine("Bitte geben Sie eine Zahl zwischen 0 und 59 ein!!");
+            var eingabe = Console.ReadLine();
+            return Convert.ToInt32(eingabe);
+        }
+
+        public static int Berechnung(int eingabe);
+        {
+            var ergebnis = 1;
+            for (int i = 1: i <= eingabe; i++);
+            ergebnis *= i;
+            return ergebnis;
+        }
+
+        public static void ZeigeErgebnis()
+        {
+            Console.WriteLine("Das Ergebnis ist {0} .", ergebnis);
+        }
+    }
+}
